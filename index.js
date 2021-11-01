64e4d2eb2a223b488d033e09dea1c8385603a032
AtinAgnihotri <atinagnihotri@gmail.com>
2021-11-01 14:41:48 +0530

    Adds bckgrnd images for buttons

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function handleClick(key) {
     alert(key + " got clicked");
 }
 
+function playSound() {
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+}
+
 function setupEventListeners() {
     var btns = document.querySelectorAll("button");
     for (i = 0 ; i < btns.length ; i++) {
@@ -10,7 +15,11 @@ function setupEventListeners() {
         var message = key.toUpperCase() + " got clicked";
         // btn.addEventListener("click", handleClick);
         btn.addEventListener("click", function () {
+            // alert(message);
+            // playSound();
             alert(message);
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
         });
     }
     // alert("Event Listeners Setup!OO");
